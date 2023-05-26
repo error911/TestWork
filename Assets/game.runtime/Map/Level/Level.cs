6eaf2c53dfad17428611c7a06a6863dbe96c2774
Roman Murnik <murnikr@gmail.com>
2023-05-26 14:06:57 +0300

    add level data

--- Assets/game.runtime/Map/Level/Level.cs
+++ Assets/game.runtime/Map/Level/Level.cs
@@ -7,18 +7,16 @@ public class Level : MonoBehaviour
     public Action<Cell> OnCellMouseEnter;
     public Action<Cell> OnCellMouseClick;
 
-    public Player PlayerA => _playerA;
-    public Player PlayerB => _playerB;
+    public Player PlayerA => _data.playerA;
+    public Player PlayerB => _data.playerB;
 
     private LevelConfig _config;
-    private Cell[,] _cells;
-    private Player _playerA;
-    private Player _playerB;
-    private List<Obstacle> _obstacles;
+    private LevelData _data;
 
     public void Construct(LevelConfig config)
     {
         _config = config;
+        _data= new LevelData();
         CreateGrid();
         CreatePlayers();
         CreateObstacles();
@@ -29,7 +27,7 @@ public class Level : MonoBehaviour
         var sizeX = _config.MapSize.x;
         var sizeY = _config.MapSize.y;
 
-        _cells = new Cell[sizeX,sizeY];
+        _data.cells = new Cell[sizeX,sizeY];
 
         for (int y = 0; y < sizeY; y++)
         {
@@ -40,25 +38,25 @@ public class Level : MonoBehaviour
                 cell.transform.localScale = new Vector3(_config.CellSize, cell.transform.localScale.y, _config.CellSize);
                 cell.transform.localScale = cell.transform.localScale * 0.95f;
                 cell.transform.position = new Vector3(x * _config.CellSize, 0, y * _config.CellSize);
-                _cells[x, y] = cell;
+                _data.cells[x, y] = cell;
             }
         }
     }
 
     private void CreatePlayers()
     {
-        _playerA = Instantiate(_config.PlayerAConfig.PlayerPref, transform);
+        _data.playerA = Instantiate(_config.PlayerAConfig.PlayerPref, transform);
         var cellA = GetCell(_config.PlayerAConfig.Position);
-        _playerA.SetPosition(cellA);
+        _data.playerA.SetPosition(cellA);
 
-        _playerB = Instantiate(_config.PlayerBConfig.PlayerPref, transform);
+        _data.playerB = Instantiate(_config.PlayerBConfig.PlayerPref, transform);
         var cellB = GetCell(_config.PlayerBConfig.Position);
-        _playerB.SetPosition(cellB);
+        _data.playerB.SetPosition(cellB);
     }
 
     private void CreateObstacles()
     {
-        _obstacles = new List<Obstacle>();
+        _data.obstacles = new List<Obstacle>();
     }
 
     private void OnCellMouseEnterInternal(Cell cell)
@@ -73,7 +71,7 @@ public class Level : MonoBehaviour
 
     private Cell GetCell(Vector2Int pos)
     {
-        return _cells[pos.x, pos.y];
+        return _data.cells[pos.x, pos.y];
     }
 
     public void CreateObstacle(Cell cell)
@@ -81,15 +79,15 @@ public class Level : MonoBehaviour
         var obstacle = Instantiate(_config.ObstacleConfig.ObstaclePref, transform);
         obstacle.SetPosition(cell);
 
-        _obstacles.Add(obstacle);
-        _cells[cell.position2d.x, cell.position2d.y].PlaceToCell(obstacle);
+        _data.obstacles.Add(obstacle);
+        _data.cells[cell.position2d.x, cell.position2d.y].PlaceToCell(obstacle);
     }
 
     public void RemoveObstacle(Cell cell)
     {
         var obstacle = (Obstacle)cell.placedObject;
-        _obstacles.Remove(obstacle);
-        _cells[cell.position2d.x, cell.position2d.y].PlaceToCell(null);
+        _data.obstacles.Remove(obstacle);
+        _data.cells[cell.position2d.x, cell.position2d.y].PlaceToCell(null);
         Destroy(obstacle.gameObject);
     }
 
---
+++ Assets/game.runtime/Map/Level/LevelData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public class LevelData
+{
+    public Cell[,] cells;
+    public Player playerA;
+    public Player playerB;
+    public List<Obstacle> obstacles;
+}
